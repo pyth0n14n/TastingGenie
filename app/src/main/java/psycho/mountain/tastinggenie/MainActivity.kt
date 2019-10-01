49e8619ee45cd6a0203d90096357f26b76a86d38
S.Nashimoto <jaguar1014@hotmail.com>
2019-10-01 22:20:07 +0900

    FragmentのボタンをActivityで制御し遷移することができた

--- app/src/main/java/psycho/mountain/tastinggenie/MainActivity.kt
+++ app/src/main/java/psycho/mountain/tastinggenie/MainActivity.kt
@@ -2,23 +2,40 @@ package psycho.mountain.tastinggenie
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_test_fragment.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TestFragment.FragmentListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         if (savedInstanceState == null){
             val fragmentManager = supportFragmentManager
             val fragmentTransaction = fragmentManager.beginTransaction()
 
-            // 戻り先はnull
-            fragmentTransaction.addToBackStack(null)
+            //fragmentTransaction.addToBackStack(null)
 
             fragmentTransaction.replace(R.id.container, TestFragment.newInstance())
             fragmentTransaction.commit()
-
         }
     }
+
+    override fun onClickButton() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        // 戻り先がnull菜のではなく，バックスタックに遷移を記録する（戻せるようにする）という意味
+        fragmentTransaction.addToBackStack(null)
+
+        fragmentTransaction.replace(R.id.container, TestFragment2.newInstance())
+        fragmentTransaction.commit()
+    }
+
+
+
+
 }
--- app/src/main/java/psycho/mountain/tastinggenie/TestFragment.kt
+++ app/src/main/java/psycho/mountain/tastinggenie/TestFragment.kt
@@ -1,12 +1,21 @@
 package psycho.mountain.tastinggenie
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import kotlinx.android.synthetic.main.fragment_test_fragment.*
 
 class TestFragment : Fragment() {
+
+    interface FragmentListener {
+        fun onClickButton()
+    }
+
+    private var mListener: FragmentListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,9 +25,27 @@ class TestFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_test_fragment, container, false)
     }
 
-    // singleton
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        if (context is FragmentListener){
+            mListener = context
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_test_fragment!!.setOnClickListener {
+            mListener?.let {
+                it.onClickButton()
+            }
+        }
+    }
+
+    // factory method
     companion object {
-        @JvmStatic
-        fun newInstance() : TestFragment = TestFragment();
+        fun newInstance() : TestFragment {
+            return TestFragment()
+        }
     }
 }
