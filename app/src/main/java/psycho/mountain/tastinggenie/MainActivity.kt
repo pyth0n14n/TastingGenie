d7a8603449fbda75450735ab782ea6894890a4d6
S.Nashimoto <jaguar1014@hotmail.com>
2019-10-06 20:39:22 +0900

    カメラの使い方tmp 結局解決せず

--- app/src/main/java/psycho/mountain/tastinggenie/MainActivity.kt
+++ app/src/main/java/psycho/mountain/tastinggenie/MainActivity.kt
@@ -12,10 +12,15 @@ import org.jetbrains.anko.imageBitmap
 import java.io.FileNotFoundException
 import java.io.IOException
 import java.io.InputStream
+import android.os.Build
+import android.content.ContentValues
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity(), TestFragment.TestFragmentListener, RegisterDBFragment.ImageSelectListener {
 
     val REQUEST_GET_IMAGE = 100
+    val REQUEST_CHOOSER = 1000
     val MAX_IMAGE_SIZE = 500
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,7 +54,15 @@ class MainActivity : AppCompatActivity(), TestFragment.TestFragmentListener, Reg
         val pickPhotoIntent : Intent = Intent(Intent.ACTION_GET_CONTENT)
         pickPhotoIntent.setType("image/*")
 
+        val photoName = System.currentTimeMillis().toString() + ".jpg"
+        val contentValues = ContentValues()
+        contentValues.put(MediaStore.Images.Media.TITLE, photoName)
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        val uri = contentResolver
+            .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
         val takePhotoIntent : Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
 
         val chooserIntent : Intent = Intent.createChooser(pickPhotoIntent, "Picture...")
         chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, takePhotoIntent)
@@ -57,15 +70,45 @@ class MainActivity : AppCompatActivity(), TestFragment.TestFragmentListener, Reg
         startActivityForResult(chooserIntent, REQUEST_GET_IMAGE)
     }
 
+//    private fun showGallery() {
+//        //カメラの起動Intentの用意
+//        val photoName = System.currentTimeMillis().toString() + ".jpg"
+//        val contentValues = ContentValues()
+//        contentValues.put(MediaStore.Images.Media.TITLE, photoName)
+//        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+//        val uri = contentResolver
+//            .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+//
+//        val intentCamera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+//
+//        // ギャラリー用のIntent作成
+//        val intentGallery: Intent
+//        if (Build.VERSION.SDK_INT < 19) {
+//            intentGallery = Intent(Intent.ACTION_GET_CONTENT)
+//            intentGallery.type = "image/*"
+//        } else {
+//            intentGallery = Intent(Intent.ACTION_OPEN_DOCUMENT)
+//            intentGallery.addCategory(Intent.CATEGORY_OPENABLE)
+//            intentGallery.type = "image/jpeg"
+//        }
+//        val intent = Intent.createChooser(intentCamera, "画像の選択")
+//        intent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(intentGallery))
+//        startActivityForResult(intent, REQUEST_GET_IMAGE)
+//    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == REQUEST_GET_IMAGE && resultCode == Activity.RESULT_OK && data != null){
             try {
+                Log.d("asdf", "activityResult")
                 if (data.extras != null && data.extras.get("data") != null) {
+                    Log.d("asdf", "bitmap")
                     val capturedImage: Bitmap = compressBitmap(data.extras.get("data") as Bitmap)
                     register_db_image.imageBitmap = capturedImage
                 } else {
+                    Log.d("asdf", "stream")
                     val stream: InputStream = contentResolver.openInputStream(data.data)
                     val bitmap: Bitmap = compressBitmap(BitmapFactory.decodeStream(stream))
                     stream.close()
